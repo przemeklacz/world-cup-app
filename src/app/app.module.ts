c881ef07846b504089f46ccbcda6955eb55bde29
Przemek Laczycki <przemek.laczycki@gmail.com>
2018-05-29 21:45:09 +0200

    Upgrade to Angular v6, created Auth Service, added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
+import { MaterialModule } from './../material.module';
+import { AuthService } from './services/auth.service';
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +17,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
