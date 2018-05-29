fb6a5de00f28c278be57fcd4b6b1bc7a18c596b2
Przemek Laczycki <przemek.laczycki@gmail.com>
2018-05-29 20:04:37 +0200

    Set firebase credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBD8ySNDRSCzJNHugwpBG27u1JeIAFyXDA",
+    authDomain: "world-cup-app-847b7.firebaseapp.com",
+    databaseURL: "https://world-cup-app-847b7.firebaseio.com",
+    projectId: "world-cup-app-847b7",
+    storageBucket: "world-cup-app-847b7.appspot.com",
+    messagingSenderId: "654256755384"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBD8ySNDRSCzJNHugwpBG27u1JeIAFyXDA",
+    authDomain: "world-cup-app-847b7.firebaseapp.com",
+    databaseURL: "https://world-cup-app-847b7.firebaseio.com",
+    projectId: "world-cup-app-847b7",
+    storageBucket: "world-cup-app-847b7.appspot.com",
+    messagingSenderId: "654256755384"
+  }
 };
